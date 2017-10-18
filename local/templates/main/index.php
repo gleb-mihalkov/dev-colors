0ba621307bc2424e5b2736d421fe18a3a46b6305
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-10-19 00:43:59 +0300

    Добавление ссылок-иконок

--- local/templates/main/index.php
+++ local/templates/main/index.php
@@ -81,7 +81,29 @@
                     © <?= $footerYear; ?> Краски. Все права защищены.<br>
                     Сайт сделан: <a href="http://more-use.com/">More Use</a>
                 </div>
-                <div class="footer__item"></div>
+                <div class="footer__item">
+                    <nav role="navigation" class="footer-menu">
+                        <a
+                            class="footer-menu__item"
+                            href="tel:+79205688986   "
+                            rel="nofollow"
+                            >
+                            +7 920 568 89 86
+                           </a>
+                        <a
+                            class="footer-menu__item  link link--icon link--icon-facebook"
+                            href="#"
+                        ></a>
+                        <a
+                            class="footer-menu__item  link link--icon link--icon-vk"
+                            href="#"
+                        ></a>
+                        <a
+                            class="footer-menu__item  link link--icon link--icon-instagramm"
+                            href="#"
+                        ></a>
+                    </nav>
+                </div>
             </footer>
         </div>
     </div>
