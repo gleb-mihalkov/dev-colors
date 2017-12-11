3b0f2fceaced10668e6b0b4ecfd5465474222ba1
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-12-11 05:12:39 +0300

    Заливка

--- local/templates/main/components/bitrix/news.list/about-page/template.php
+++ local/templates/main/components/bitrix/news.list/about-page/template.php
@@ -12,36 +12,44 @@
 ?>
 <article class="about-page">
     <h1 class="about-page__title">О школе</h1>
-    <section class="about-page__group-01" id="<?= $itemA->getEditId($this); ?>">
+    <section class="about-page__group-01  not-viewed" id="<?= $itemA->getEditId($this); ?>">
         <div class="about-page__image-group-01">
             <div
                 style="background-image: url(<?= $itemA->image; ?>)"
                 class="about-page__image-01"
                 ></div>
         </div>
-        <div class="about-page__text-01"><?= $itemA->desc; ?></div>
+        <div class="about-page__text-01">
+            <span><?= $itemA->desc; ?></span>
+        </div>
     </section>
-    <section class="about-page__group-02" id="<?= $itemB->getEditId($this); ?>">
+    <section class="about-page__group-02  not-viewed" id="<?= $itemB->getEditId($this); ?>">
         <div class="about-page__text-02"><?= $itemB->desc; ?></div>
     </section>
-    <section class="about-page__group-03" id="<?= $itemC->getEditId($this); ?>">
-        <div class="about-page__text-03"><?= $itemC->desc; ?></div>
+    <section class="about-page__group-03  not-viewed" id="<?= $itemC->getEditId($this); ?>">
+        <div class="about-page__text-03">
+            <span><?= $itemC->desc; ?></span>
+        </div>
     </section>
-    <section class="about-page__group-04" id="<?= $itemD->getEditId($this); ?>">
+    <section class="about-page__group-04  not-viewed" id="<?= $itemD->getEditId($this); ?>">
         <div
             style="background-image: url(<?= $itemD->image; ?>)"
             class="about-page__image-04"
             ></div>
-        <div class="about-page__text-04"><?= $itemD->desc; ?></div>
+        <div class="about-page__text-04">
+            <span><?= $itemD->desc; ?></span>
+        </div>
     </section>
-    <div class="about-page__group-05"></div>
-    <section class="about-page__group-06" id="<?= $itemE->getEditId($this); ?>">
+    <div class="about-page__group-05  not-viewed"></div>
+    <section class="about-page__group-06  not-viewed" id="<?= $itemE->getEditId($this); ?>">
         <div class="about-page__image-group-06">
             <div
                 style="background-image: url(<?= $itemE->image; ?>)"
                 class="about-page__image-06"
                 ></div>
         </div>
-        <div class="about-page__text-06"><?= $itemE->desc; ?></div>
+        <div class="about-page__text-06  not-viewed">
+            <span><?= $itemE->desc; ?></span>
+        </div>
     </section>
 </article>
