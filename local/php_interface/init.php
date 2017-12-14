9c5da42c34bab067a4aecb9ebf1fccfe8c127cf4
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-12-14 13:06:29 +0300

    Заливка.

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -5,5 +5,10 @@ AddEventHandler('main', 'OnBeforeProlog', function() {
     $host = $_SERVER['HTTP_HOST'];
     if ($host !== 'kraskipro.com') return;
 
-    LocalRedirect('site-not-working.php');
+    $redirectPage = '/site-not-working.php';
+
+    $file = $_SERVER['PHP_SELF'];
+    if ($file == $redirectPage) return;
+
+    LocalRedirect($redirectPage);
 });
