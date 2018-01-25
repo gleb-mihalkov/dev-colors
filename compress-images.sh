29b1f6bd1f31a9ee39f1a70c4800145c2e2a5b50
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2018-01-25 23:16:14 +0300

    Правка скрипта для оптимизации изображений

--- compress-images.sh
+++ compress-images.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-TEMPLATE_DIR='./local/templates/main/img'
-UPLOAD_DIR='./upload'
+TEMPLATE_DIR="./local/templates/main/img"
+UPLOAD_DIR="./upload"
 
-find "$TEMPLATE_DIR" "$UPLOAD_DIR" -type f \( -name "*.jpg" -o -name "*.JPG" -o -name "*.jpeg" -o -name "*.JPEG" \) \
+find $TEMPLATE_DIR $UPLOAD_DIR -type f \( -name "*.jpg" -o -name "*.JPG" -o -name "*.jpeg" -o -name "*.JPEG" \) \
   -exec jpegoptim -o -t -m89 --all-progressive --strip-all '{}' \;
 
-find "$TEMPATE_DIR" "$UPLOAD_DIR" -type f \( -name "*.png" -o -name "*.PNG" \) \
-  -exec optipng -o7 '{}' \;
+find $TEMPATE_DIR $UPLOAD_DIR -type f \( -name "*.png" -o -name "*.PNG" \) \
+  -exec optipng '{}' \;
