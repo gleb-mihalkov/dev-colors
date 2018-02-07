084d839c3957d72dc75493d1974ce68273f3bdeb
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2018-02-07 14:49:53 +0300

    Отмена блокировки основного домена

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -5,10 +5,10 @@ AddEventHandler('main', 'OnBeforeProlog', function() {
     $host = $_SERVER['HTTP_HOST'];
     if ($host !== 'kraskipro.com') return;
 
-    $redirectPage = '/site-not-working.php';
+    // $redirectPage = '/site-not-working.php';
 
-    $file = $_SERVER['PHP_SELF'];
-    if ($file == $redirectPage) return;
+    // $file = $_SERVER['PHP_SELF'];
+    // if ($file == $redirectPage) return;
 
-    LocalRedirect($redirectPage);
+    // LocalRedirect($redirectPage);
 });
