b65c1ad1db57ee1a3f9d2a0ad03aff5c0f4d68bd
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2018-02-15 05:07:58 +0300

    Правка бага анимации слайдера на главной в MS Edge.

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -33,6 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width; initial-scale=1.0; user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="shortcut icon" href="/favicon.ico?v=2">
     <title><?= $APPLICATION->ShowTitle(); ?></title>
     <?
